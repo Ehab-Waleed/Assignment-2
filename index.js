597a998372d852d169c379123fac5be0d9d2381c
Ehab-Waleed <xehabwaleed@gmail.com>
2026-06-16 10:38:47 +0300

    Question 2

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 // 1- //
-getFileDir = () => {
+const getFileDir = () => {
 return {  Dir: __dirname , File: __filename }
 }
 console.log(getFileDir());
 
-/ ***************************************************** */
+// ***************************************************** *//
 
+// 2- // 
+const path = require("node:path")
+const resolvedPath = path.resolve("index.js")
+const getFileName = (filepath) => {
+     return path.basename(filepath)   
+}
+console.log(getFileName(resolvedPath));
+
+// ***************************************************** *//
+
+
+   
+   
